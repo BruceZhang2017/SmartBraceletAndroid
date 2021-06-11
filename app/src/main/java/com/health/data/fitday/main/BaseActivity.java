0748bc5a08b93f0ca312e09729d0524beeeec695
BruceZhang2017 <bruce.zhang@oceanwing.com>
2021-06-11 20:47:59 +0800

    修改状态栏背景颜色问题。

--- app/src/main/java/com/health/data/fitday/main/BaseActivity.java
+++ app/src/main/java/com/health/data/fitday/main/BaseActivity.java
@@ -9,6 +9,9 @@ import android.view.Window;
 import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.health.data.fitday.global.StatusBarUtil;
+import com.sinophy.smartbracelet.R;
+
 public abstract class BaseActivity extends AppCompatActivity {
     protected abstract int getLayoutId();
 
@@ -17,11 +20,11 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected abstract void initView();
 
     protected void onCreate(Bundle paramBundle) {
-        setTransparentStatusBar();
         super.onCreate(paramBundle);
         setContentView(getLayoutId());
         initView();
         initData();
+        StatusBarUtil.setStatusBarMode(this, true, R.color.white);
     }
 
     protected void onDestroy() {
@@ -42,19 +45,6 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected void onStop() {
         super.onStop();
-    }
-
-    public void setTransparentStatusBar() {
-        Window window = getWindow();
-        // 设置状态栏全透明
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            //window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
     }
 
 }
